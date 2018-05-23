6aa5bd16d2bfe0e28a69ffbb914c70efdccde94e
rafaspita <rafael.spitaliere@gmail.com>
2018-05-23 11:10:18 -0300

    projeto adicionado ao fabric

--- InternalTimeSheet/app/src/main/java/br/com/accera/internaltimesheet/DashboardActivity.java
+++ InternalTimeSheet/app/src/main/java/br/com/accera/internaltimesheet/DashboardActivity.java
@@ -2,14 +2,18 @@ package br.com.accera.internaltimesheet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.joaquimley.faboptions.FabOptions;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class DashboardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
+        Fabric.with(this, new Crashlytics());
         setContentView( R.layout.activity_dashboard );
     }
 }
